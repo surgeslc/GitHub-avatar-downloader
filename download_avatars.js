80596adb5689bc0f78bf15225b7de83ba5ae5bfd
Lawrence Surges <surgeslc@gmail.com>
2017-07-04 19:08:16 +0000

    At end of Step 3

--- download_avatars.js
+++ download_avatars.js
@@ -2,6 +2,16 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+  getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+}
+
+
+
 // USAGE
 // Should be executed from the command line, eg
 // node download_avatars.js jquery jquery

cbda180c44b24230f6510f99bfacc082f54fdad1
Lawrence Surges <surgeslc@gmail.com>
2017-07-04 19:46:34 +0000

    At end of Step 4

--- download_avatars.js
+++ download_avatars.js
@@ -2,13 +2,22 @@ var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
+var GITHUB_USER = "surgeslc";
+var GITHUB_TOKEN = "4f7d68aed0e41c520272e7e14f55035f395fcaec";
+
 function getRepoContributors(repoOwner, repoName, cb) {
   // ...
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL);
+
+
+}
+
   getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
-}
+//getRepoContributors("surgeslc", "request");
 
 
 
